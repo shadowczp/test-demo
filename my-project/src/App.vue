cc252e740160e443db4def761f1ecfcc14c086c5
shadowczp <695034851@qq.com>
2018-07-01 22:24:06 +0800

    20180701

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -3,23 +3,29 @@
     <img src="./assets/logo.png">
     <router-link to="/a">Go to Foo</router-link>
     <router-link to="/b">Go to Bar</router-link>
+    <button @click="forward">测试一下跳转</button>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  export default {
+    name: 'App',
+    methods: {
+      forward(){
+        this.$router.push('/a5456')
+      }
+    }
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
